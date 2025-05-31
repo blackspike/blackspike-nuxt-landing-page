9433aaf4028754ee99e0cb5dcb059955c9c45dc7
Felix Corke <felix@blackspike.com>
2025-05-31 14:04:32 +0300

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
   devtools: { enabled: true },
+  ssr: true,
   modules: ['@nuxt/fonts', '@nuxt/image', 'nuxt-svgo-loader'],
   css: [
     '~/assets/css/base.css',
@@ -14,6 +15,11 @@ export default defineNuxtConfig({
     '~/assets/css/layout.css',
     '~/assets/css/typography.css',
   ],
+  fonts: {
+    defaults: {
+      weights: [400, 500, 600, 700]
+    },
+  },
   vite: {
     plugins: [
       tailwindcss(),
